84e696c25fbec8e4bf616f17b26b3be6d4597a57
Young <mcpjjh@gmail.com>
2017-01-16 17:29:01 +0000

    step 2: printing with buffering strings

--- step2.js
+++ step2.js
@@ -1,27 +1,30 @@
 var https = require('https');
 
-function getAndPrintHTMLChunks () {
+function getAndPrintHTML () {
 
 // var https = require('https');
 
   var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
 
 https.get(requestOptions, function (response) {
+
+  var finalData = "";
 
   response.setEncoding('utf8');
 
   response.on('data', function (data) {
     console.log('Chunk Received:', data);
+    finalData += data
   })
 
   response.on('end', function() {
-    console.log('Response complete.');
+    console.log(finalData);
   });
 
   })
 }
 
-getAndPrintHTMLChunks();
+getAndPrintHTML();
